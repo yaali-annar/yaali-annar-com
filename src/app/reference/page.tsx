2f2a71f6f7fd9bfeedd68e1e5f2d7e80dff8ce96
yaali-annar <pyurio@gmail.com>
2024-09-18 17:49:01 +0700

    feat: clothed version of reference

--- src/app/reference/page.tsx
+++ src/app/reference/page.tsx
@@ -2,41 +2,62 @@
 
 import NavBar from "@/components/navbar";
 import { createAccessibleProps } from "@/utils/function";
-import { classNames } from "@/utils/string";
+import { classNames, kebabToProperName } from "@/utils/string";
 import { useState } from "react";
 
 const images = ["front", "front-side", "side", "back-side", "back"];
+const versions = ["clothed", "natural"];
 
 const Reference = () => {
+  const [selectedVersion, setSelectedVersion] = useState("clothed")
   const [selectedIndex, setSelectedIndex] = useState(0);
   const selectedImage = images[selectedIndex];
 
   return (
     <>
       <NavBar />
-      <main className="flex flex-col items-center justify-center bg-black text-yellow-400 w-full min-h-screen p-4">
+      <main className="flex flex-col items-center justify-center w-full min-h-screen p-4 gap-4 lg:gap-6">
         <h1>Presenting The Babi</h1>
-        <div className="lg:flex">
+        <div className="flex justify-center gap-2">
+          {versions.map(version => <button
+            {...createAccessibleProps(() => setSelectedVersion(version), ['Enter', ' '])}
+
+            key={version}
+            className={classNames('rounded-full px-2', {
+              'bg-yellow-400 text-black': selectedVersion === version,
+              'border border-yellow-400': selectedVersion === version
+            })}
+
+          >{kebabToProperName(version)}</button>)}
+        </div>
+        <div className="flex flex-col lg:flex-row gap-4">
           <div className="flex mb-4 justify-center lg:flex-col">
             {images.map((image, index) => (
               <button
                 {...createAccessibleProps(() => setSelectedIndex(index), ['Enter', ' '])}
                 key={image}
-                className={classNames('w-1/5 lg:w-full max-w-16 rounded-lg', { 'border-2 border-yellow-400': selectedIndex === index })}
+                className={classNames('border-2 w-1/5 lg:w-full lg:h-1/5 rounded-lg p-1 flex justify-center items-center', {
+                  'border-yellow-400': selectedIndex === index,
+                  'border-black': selectedIndex !== index
+                })}
               >
                 <img
-                  className="max-h-28"
-                  src={`/reference/yaali-${image}.png`}
+                  className="max-h-28 object-contain"
+                  src={`/reference/yaali-${image}-${selectedVersion}.webp`}
                   alt="the babi"
                 />
               </button>
             ))}
           </div>
-          <img
-            className="w-full max-w-[360px] max-h-[640px] object-contain"
-            src={`/reference/yaali-${selectedImage}.png`}
-            alt="the babi"
-          />
+          <div className="max-w-[360px] max-h-[640px]"
+            style={{ width: 'calc(100vw - 32px)' }}
+          >
+            <img
+              className="object-contain max-w-[360px] max-h-[640px]"
+              src={`/reference/yaali-${selectedImage}-${selectedVersion}.webp`}
+              alt="the babi"
+            />
+          </div>
         </div>
       </main>
     </>
